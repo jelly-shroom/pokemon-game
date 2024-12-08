9c13ae7e043723b02a861f3fd2de4852b55d83ca
jellyshroom <kihyvr@gmail.com>
2024-12-08 13:21:08 -0800

    Squashed commit of the following:
    commit a3a2d6682da1cdeb1f2e533741f290645934b3b8
    Author: jellyshroom <kihyvr@gmail.com>
    Date:   Sun Dec 8 13:20:45 2024 -0800
    
        working main and login screen

--- core/src/main/java/me/jessicasheng/pokemonGame/controller/Main.java
+++ core/src/main/java/me/jessicasheng/pokemonGame/controller/Main.java
@@ -19,14 +19,16 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import me.jessicasheng.pokemonGame.view.LoginScreen;
 
 import static com.badlogic.gdx.scenes.scene2d.actions.Actions.run;
+import me.jessicasheng.pokemonGame.view.MainMenu;
+
 
 public class Main extends Game {
-    private Stage stage;
-    private Skin skin;
 
-    private TextButton loginButton, registerButton;
+
     private final Game app = this;
 
+    private Screen mainMenuScreen;
+    private Screen loginScreen;
 
     @Override
     public void setScreen(Screen screen){
@@ -36,68 +38,49 @@ public class Main extends Game {
             old.dispose();
         }
     }
-    public void toLogin() {
-        app.setScreen(new LoginScreen(this));
-    }
+
 
     @Override
     public void create() {
         GdxNativesLoader.load();
 
-        stage = new Stage(new FitViewport(640, 480));
-        skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+        mainMenuScreen = new MainMenu(this);
+        loginScreen = new LoginScreen(this);
+        setScreen(mainMenuScreen);
+//        window.pack();
+//        // We round the window position to avoid awkward half-pixel artifacts.
+//        // Casting using (int) would also work.
+//        window.setPosition(MathUtils.roundPositive(stage.getWidth() / 2f - window.getWidth() / 2f),
+//            MathUtils.roundPositive(stage.getHeight() / 2f - window.getHeight() / 2f));
+//        window.addAction(Actions.sequence(Actions.alpha(0f), Actions.fadeIn(1f)));
+//        stage.addActor(window);
 
-        Window window = new Window("Log in or register", skin, "border");
-        window.defaults().pad(8f);
-        window.add("Welcome to the Pokemon game!").row();
 
-        loginButton = new TextButton("Login", skin);
-        registerButton = new TextButton("Register", skin);
-        loginButton.pad(8f);
-        registerButton.pad(8f);
+    }
 
-        loginButton.addListener(new ChangeListener() {
-            @Override
-            public void changed(final ChangeEvent event, final Actor actor) {
-                toLogin();
-            }
-        });
+    public void toLogin() {
+        setScreen(loginScreen);
+    }
 
-        HorizontalGroup buttons = new HorizontalGroup().space(8f);
-        window.add(buttons).row();
-
-        buttons.addActor(loginButton);
-        buttons.addActor(registerButton);
-
-        window.pack();
-        // We round the window position to avoid awkward half-pixel artifacts.
-        // Casting using (int) would also work.
-        window.setPosition(MathUtils.roundPositive(stage.getWidth() / 2f - window.getWidth() / 2f),
-            MathUtils.roundPositive(stage.getHeight() / 2f - window.getHeight() / 2f));
-        window.addAction(Actions.sequence(Actions.alpha(0f), Actions.fadeIn(1f)));
-        stage.addActor(window);
-
-        Gdx.input.setInputProcessor(stage);
-
+    public void toMainMenu() {
+        setScreen(mainMenuScreen);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0f, 0f, 0f, 1f);
         super.render();
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height);
     }
 
     @Override
     public void dispose() {
-        stage.dispose();
-        skin.dispose();
+        super.dispose();
+        mainMenuScreen.dispose();
+        loginScreen.dispose();
     }
 
 }
